ce9e13b81d37bdfea512f16d92faa0c636d5803c
diegomartin.perez <diegomartin.perez@bbva.com>
2025-06-11 19:02:33 -0300

    Agrego concurrencia para realizar task asincronas en loggin y register

--- src/main/java/com/Java2/proyecto/diego/AlkemyDiego/security/controller/AuthController.java
+++ src/main/java/com/Java2/proyecto/diego/AlkemyDiego/security/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.Java2.proyecto.diego.AlkemyDiego.security.dto.AuthResponse;
 import com.Java2.proyecto.diego.AlkemyDiego.security.dto.LoginRequest;
 import com.Java2.proyecto.diego.AlkemyDiego.security.dto.RegisterRequest;
 import com.Java2.proyecto.diego.AlkemyDiego.security.service.AuthService;
+import com.Java2.proyecto.diego.AlkemyDiego.service.AsyncTaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,24 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
   private final AuthService authService;
+  private final AsyncTaskService asyncTaskService;
 
   @PostMapping("/login")
   public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
     try {
       AuthResponse response = authService.login(loginRequest);
+      asyncTaskService.logUserLogin(loginRequest.getUsername());
       return ResponseEntity.ok(response);
     } catch (RuntimeException e) {
       return ResponseEntity.status(401).body(e.getMessage());
     }
   }
 
-@PostMapping("/register")
-public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
+  @PostMapping("/register")
+  public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
     try {
-        AuthResponse response = authService.register(registerRequest);
-        return ResponseEntity.ok(response);
+      AuthResponse response = authService.register(registerRequest);
+      asyncTaskService.logUserRegistration(registerRequest.getUsername());
+      return ResponseEntity.ok(response);
     } catch (RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+      return ResponseEntity.badRequest().body(e.getMessage());
     }
-}
+  }
 }
